f4c5d5c25fc1f43bc1f14e3fb9c123370d6d7206
Fahim047 <fahimulislam58@gmail.com>
2024-11-16 17:04:54 +0600

    ✅ Delete Question Functionality Added

--- src/components/QuestionCard.jsx
+++ src/components/QuestionCard.jsx
@@ -1,5 +1,27 @@
+import Swal from 'sweetalert2';
+import { useAxios } from '../hooks';
 const QuestionCard = ({ quiz, index }) => {
 	console.log(quiz);
+	const { api } = useAxios();
+	const handleDelete = () => {
+		const deleteQuestion = async () => {
+			const response = await api.delete(`api/admin/questions/${quiz.id}`);
+			if (response.status === 200) {
+				Swal.fire('Deleted!', 'The quiz has been deleted.', 'success');
+			}
+		};
+		Swal.fire({
+			title: 'Are you sure!',
+			text: 'Do you want to continue',
+			icon: 'warning',
+			confirmButtonText: 'confirm',
+			showCancelButton: true,
+		}).then(({ isConfirmed }) => {
+			if (isConfirmed) {
+				deleteQuestion();
+			}
+		});
+	};
 	return (
 		<div className="rounded-lg overflow-hidden shadow-sm mb-4">
 			<div className="bg-white p-6 !pb-2">
@@ -41,7 +63,10 @@ const QuestionCard = ({ quiz, index }) => {
 				</div>
 			</div>
 			<div className="flex space-x-4 bg-primary/10 px-6 py-2">
-				<button className="text-red-600 hover:text-red-800 font-medium">
+				<button
+					className="text-red-600 hover:text-red-800 font-medium"
+					onClick={handleDelete}
+				>
 					Delete
 				</button>
 				<button className="text-primary hover:text-primary/80 font-medium">
